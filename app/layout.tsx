2a904f0d5ba63d5c862f4015b92b83d04a2e53e6
Trans7 IT Sofware Develover 4 <shtpersn@gmail.com>
2025-08-01 17:57:48 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
-export default function RootLayout({
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayaout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
